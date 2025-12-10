43e03e4a27d3a87ab78467a22c31c8e56317a2fa
Frieren <95279880+KinasTomes@users.noreply.github.com>
2025-12-10 10:42:03 +0700

    Delete

--- community-service/server.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import app from "./app.js";
-
-const PORT = process.env.PORT || 8004;
-
-app.listen(PORT, () => {
-  console.log(`Community service đang chạy trên cổng ${PORT}`);
-});
--- community-service/src/middlewares/index.js
+++
@@ -1,2 +0,0 @@
-export { authenticate, optionalAuthenticate } from "./authMiddleware.js";
-
